b25267133965e9b151dce01454def9a734e938e3
Nico <nicolas.jouanin@gmail.com>
2013-01-16 22:34:51 +0100

    Affichage du profil et de la teinte

--- main.py
+++ main.py
@@ -793,7 +793,7 @@ class AppWindow(QtGui.QMainWindow,Ui_MainWindow):
     def colorPreview (self) :
         self.colorTuppleSrm = ('FFE699', 'FFD878', 'FFCA5A', 'FFBF42', 'FBB123', 'F8A600', 'F39C00', 'EA8F00', 'E58500', 'DE7C00', 'D77200', 'CF6900', 'CB6200', 'C35900','BB5100', 'B54C00', 'B04500', 'A63E00', 'A13700', '9B3200', '952D00', '8E2900', '882300', '821E00', '7B1A00', '771900', '701400', '6A0E00', '660D00','5E0B00','5A0A02','600903', '520907', '4C0505', '470606', '440607', '3F0708', '3B0607', '3A070B', '36080A')
         
-        colorRef= round(self.EBC/1.97)
+        colorRef= round(self.recipe.compute_EBC()/1.97)
         
         if colorRef >= 30 :
             color = "#" + self.colorTuppleSrm[30]
@@ -1641,6 +1641,8 @@ class AppWindow(QtGui.QMainWindow,Ui_MainWindow):
         else :
             self.comboBoxType.setCurrentIndex(0)
         self.lineEditBrewer.setText(self.recipe.brewer)
+        self.displayProfile()
+        self.colorPreview()
         
         self.currentRecipeMash = self.recipe.mash
         try :
