aab3ea78a387d4f1a3a69c4d1eb3db9206d6f5cd
314r <contact.314r@gmail.com>
2013-01-23 19:56:50 +0100

    modif a propos

--- main.py
+++ main.py
@@ -2375,7 +2375,7 @@ class AppWindow(QtGui.QMainWindow,Ui_MainWindow):
     def about(self) : 
         QtGui.QMessageBox.about(self,
                 self.trUtf8("A propos"),
-                self.trUtf8("<h1>JolieBulle</h1> <b>version 2.8</b><br/>copyright (c) 2010-2013 Pierre Tavares. Thomas Gerbet et Nicolas Jouanin ont également participé au développement de cette version.<p> JolieBulle est un logiciel de lecture et de formulation de recettes de brassage.</p><p><a href =http://www.gnu.org/licenses/gpl-3.0.html>Licence : Version 3 de la Licence Générale Publique GNU</a></p><p>Certaines icônes proviennent du pack Faenza par Tiheum (Matthieu James), également distribué sous licence GPL.</p>"))
+                self.trUtf8("<h1>JolieBulle</h1> <b>version 2.8</b><br/>copyright (c) 2010-2013 Pierre Tavares.<p> JolieBulle est un logiciel de lecture et de formulation de recettes de brassage.</p><p><a href =http://www.gnu.org/licenses/gpl-3.0.html>Licence : Version 3 de la Licence Générale Publique GNU</a></p><p>Certaines icônes proviennent du pack Faenza par Tiheum (Matthieu James), également distribué sous licence GPL.</p> <p>Auteurs : Pierre Tavares, Nicolas Jouanin, Thomas Gerbet</p>"))
         
             
     def rendemt_changed(self) :
