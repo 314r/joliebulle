c0467045d42399b5cd6b14efeec5379fb093f8b5
Nico <nicolas.jouanin@gmail.com>
2013-01-26 21:12:02 +0100

    Passage en 2.8

--- dist/Mac/createDMG.sh
+++ dist/Mac/createDMG.sh
@@ -1,1 +1,1 @@
-hdiutil create -imagekey zlib-level=9 -volname Joliebulle_2.7.0 -srcfolder dist/ Joliebulle_2.7.0.dmg
+hdiutil create -imagekey zlib-level=9 -volname Joliebulle_2.8.0 -srcfolder ../dist/ Joliebulle_2.8.0.dmg
