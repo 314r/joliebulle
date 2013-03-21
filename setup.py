5fe23b14761935a99bcc226bc859c99071129038
Le suisse <lesuisse.dev@gmail.com>
2013-03-21 23:54:38 +0100

    Prise en compte des nouveaux répertoires dans le setup.py

--- setup.py
+++ setup.py
@@ -6,33 +6,43 @@ NAME = "Joliebulle"
 VERSION = "2.8.0"
 
 setup_info = dict(
-      name = NAME,
-      version = VERSION,
-      app = APP,
-      license='GPLv3',
-      options=dict(
-	py2app=dict(
-		iconfile='dist/Mac/bulle.icns',
-		plist=dict(
-			CFBundleName               = NAME,
-			CFBundleShortVersionString = VERSION,     # must be in X.X.X format
-			CFBundleGetInfoString      = NAME + " " + VERSION,
-			CFBundleExecutable         = NAME,
-			CFBundleIdentifier         = "com.joliebulle",
-			LSEnvironment			   = {'LC_ALL':'en_US.UTF-8'}
-		),
-		argv_emulation=True,
-		includes=['sip', 'PyQt4','PyQt4.QtNetwork'],
-	),
-      ),
-      description = "JolieBulle, logiciel de brassage libre",
-      author = "Pierre Tavares",
-      author_email = "contact.314r@gmail.com",
-      url = "http://joliebulle.tuxfamily.org",
-      package_data = { "joliebulle":["*.py", "*.qm", "*.xml", "Images/*.png", "Samples/*.xml"] },
-      setup_requires=['py2app'],
-      scripts = ["joliebulle/joliebulle"],
-      iconfile='joliebulle/Images/bulle.png'       
+    name = NAME,
+    version = VERSION,
+    app = APP,
+    license='GPLv3',
+    options=dict(
+        py2app=dict(
+            iconfile='dist/Mac/bulle.icns',
+            plist=dict(
+                CFBundleName               = NAME,
+                CFBundleShortVersionString = VERSION,     # must be in X.X.X format
+                CFBundleGetInfoString      = NAME + " " + VERSION,
+                CFBundleExecutable         = NAME,
+                CFBundleIdentifier         = "com.joliebulle",
+                LSEnvironment			   = {'LC_ALL':'en_US.UTF-8'}
+            ),
+            argv_emulation=True,
+            includes=['sip', 'PyQt4','PyQt4.QtNetwork'],
+        ),
+    ),
+    description = "JolieBulle, logiciel de brassage libre",
+    author = "Pierre Tavares",
+    author_email = "contact.314r@gmail.com",
+    url = "http://joliebulle.tuxfamily.org",
+    package_data = {
+        "joliebulle":[
+            "*.py",
+            "*.qm",
+            "*.xml",
+            "Images/*.png",
+            "Samples/*.xml",
+            "*/*.py",
+            "*/*/*.py"
+        ]
+    },
+    setup_requires=['py2app'],
+    scripts = ["bin/joliebulle"],
+    iconfile='joliebulle/Images/bulle.png'       
 )  
 
 from sys import platform
