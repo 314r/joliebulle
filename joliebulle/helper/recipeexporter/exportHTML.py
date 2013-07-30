f8b16d13517f9c0254b732b5d6b030adf71241a9
314r <contact.314r@gmail.com>
2013-07-31 01:29:33 +0200

    integration

--- joliebulle/helper/recipeexporter/exportHTML.py
+++ joliebulle/helper/recipeexporter/exportHTML.py
@@ -35,12 +35,15 @@ def exportHTML(recipe):
 <head>
 <title>%s</title>
 <meta charset="utf-8" />
-<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 <style type="text/css">
-html { font-size:100.01%%; background-color : white;}
-body {width:800px;margin:auto;line-height: 1.5;color: #222; font-size:80%%; background-color : white;}
+.container,
+.navbar-static-top .container,
+.navbar-fixed-top .container,
+.navbar-fixed-bottom .container {width: 800px;}
+body {padding-top : 60px;color: #222; font-size:80%%; background-color : white;}
 h1,h2,h3,h4,h5,h6 { font-weight: normal; color: #111; }
-h1 { font-size: 2em; margin-bottom: 0; text-align:center;}
+/*h1 { font-size: 2em; margin-bottom: 0; text-align:center;}*/
 h2 { font-size: 1.5em; line-height: 1; margin-bottom: 2em; margin-top:2em; padding-bottom:0.75em; padding-top:0.75em;border-bottom:solid 1px #ddd;clear:both;}
 h3 { font-size: 1.2em; line-height: 1.25; margin-bottom: 1.25em; text-align:left; background-color:#eeeeee; border-bottom:1px solid #cccccc; border-top:1px solid #cccccc; padding:0.5em 0 0.5em 0.5em;}
 ul{list-style-type: none;text-align:left;}
@@ -50,45 +53,70 @@ pre {white-space: pre-wrap;font-size:1.25em;}
 .profil td {min-width: 200px;}
 .ingredients{text-align:left; margin-bottom:2em;}
 .ingredients td {min-width: 200px;}
-
-.footer { width:700px;
-margin:auto; 
-margin-top:4em;
-margin-bottom:4em;
-padding:0.5em;
-background-color:#eeeeee;
-border-bottom :1px solid #cccccc;
-border-top :1px solid #cccccc;
-text-align : center;}
+.profilNav{padding-right: 2.5em;}
+.context{padding-right: 2.5em;}
+.info{margin-bottom:2.75em; margin-top:1.75em;}
+.navbar-inner{background-color: white;background-image: none;}
+.profil2{ text-align: center}
+.navbar-inner .brand {color:#222;}
+#brewChart {padding-top :1.5em;}
 </style>
 </head>
-<body>
-<h1>%s</h1>
-<h2 class="genre">%s</h2>''' % (recipe.name, recipe.name, recipe.style)
+<body onload="createChart();">
+''' % (recipe.name)
 
     #Profil
-    resultHtml += '<table class="profil">'
-    resultHtml += '<tr><td>%s</td><td>%.1f %%</td></tr>' % (QCoreApplication.translate("Export", "Rendement : ", None, QCoreApplication.UnicodeUTF8), recipe.efficiency)
-    resultHtml += '<tr><td>%s</td><td>%.0f min</td></tr>' % (QCoreApplication.translate("Export", "Ébullition : ", None, QCoreApplication.UnicodeUTF8), recipe.boil)
-    resultHtml += '<tr><td>%s</td><td>%.3f</td></tr>' % (QCoreApplication.translate("Export", "Densité initiale : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_OG())
-    resultHtml += '<tr><td>%s</td><td>%.3f</td></tr>' % (QCoreApplication.translate("Export", "Densité finale : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_FG())
-    resultHtml += '<tr><td>%s</td><td>%.0f EBC</td></tr>' % (QCoreApplication.translate("Export", "Teinte : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_EBC())
-    resultHtml += '<tr><td>%s</td><td>%.0f IBU</td></tr>' % (QCoreApplication.translate("Export", "Amertume : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_IBU())
-    resultHtml += '<tr><td>%s</td><td>%.1f</td></tr>' % (QCoreApplication.translate("Export", "Ratio BU/GU : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_ratioBUGU())
-    resultHtml += '<tr><td>%s</td><td>%.1f %%</td></tr>' % (QCoreApplication.translate("Export", "Alcool (vol) : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_ABV())
-    resultHtml += '</table>'
+    # resultHtml += '<table class="profil">'
+    # resultHtml += '<tr><td>%s</td><td>%.1f %%</td></tr>' % (QCoreApplication.translate("Export", "Rendement : ", None, QCoreApplication.UnicodeUTF8), recipe.efficiency)
+    # resultHtml += '<tr><td>%s</td><td>%.0f min</td></tr>' % (QCoreApplication.translate("Export", "Ébullition : ", None, QCoreApplication.UnicodeUTF8), recipe.boil)
+    # resultHtml += '<tr><td>%s</td><td>%.3f</td></tr>' % (QCoreApplication.translate("Export", "Densité initiale : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_OG())
+    # resultHtml += '<tr><td>%s</td><td>%.3f</td></tr>' % (QCoreApplication.translate("Export", "Densité finale : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_FG())
+    # resultHtml += '<tr><td>%s</td><td>%.0f EBC</td></tr>' % (QCoreApplication.translate("Export", "Teinte : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_EBC())
+    # resultHtml += '<tr><td>%s</td><td>%.0f IBU</td></tr>' % (QCoreApplication.translate("Export", "Amertume : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_IBU())
+    # resultHtml += '<tr><td>%s</td><td>%.1f</td></tr>' % (QCoreApplication.translate("Export", "Ratio BU/GU : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_ratioBUGU())
+    # resultHtml += '<tr><td>%s</td><td>%.1f %%</td></tr>' % (QCoreApplication.translate("Export", "Alcool (vol) : ", None, QCoreApplication.UnicodeUTF8), recipe.compute_ABV())
+    # resultHtml += '</table>'
+
+    #Navbar
+    resultHtml += '''<div class="navbar navbar-fixed-top">
+                        <div class="navbar-inner">
+                            <div class="container">
+                                <a class="brand" href="#">%s</a>
+                                <div class="profil2 pull-right">                
+                                    <span class="navbar-text profilNav" data-toggle="tooltip" data-placement="bottom" title="Amertume">%.0f IBU</span>
+                                    <span class="navbar-text profilNav" data-toggle="tooltip" data-placement="bottom" title="Teinte">%.0f EBC</span>
+                                    <span class="navbar-text profilNav" data-toggle="tooltip" data-placement="bottom" title="Densité initiale">DI %.3f</span>
+                                    <span class="navbar-text profilNav" data-toggle="tooltip" data-placement="bottom" title="Densité finale">DF %.3f</span>
+                                    <span class="navbar-text profilNav" data-toggle="tooltip" data-placement="bottom" title="Ratio BU/GU">BU/GU %.1f</span>
+                                    <span class="navbar-text" data-toggle="tooltip" data-placement="bottom" title="Taux d'alcool">Alc %.1f%%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>''' % (recipe.name,recipe.compute_IBU(), recipe.compute_EBC(), recipe.compute_OG(), recipe.compute_FG(),recipe.compute_ratioBUGU(),recipe.compute_ABV())
+
+
 
     #Ingredients informations
-    resultHtml += '<h2>%s %.1f %s</h2>' % (
-        QCoreApplication.translate("Export", "Ingrédients prévus pour un brassin de", None, QCoreApplication.UnicodeUTF8),
-        recipe.volume, QCoreApplication.translate("Export", "litres", None, QCoreApplication.UnicodeUTF8))
+    # resultHtml += '<h2>%s %.1f %s</h2>' % (
+    #     QCoreApplication.translate("Export", "Ingrédients prévus pour un brassin de", None, QCoreApplication.UnicodeUTF8),
+    #     recipe.volume, QCoreApplication.translate("Export", "litres", None, QCoreApplication.UnicodeUTF8))
+
+    #Container
+    resultHtml += ''' <div class="container"> '''
+
+    resultHtml += '''<div class="info">
+                        <span class="context">Style : <span class="badge">%s</span></span>
+                        <span class="context">Rendement : <span class="badge">%.1f%%</span></span>
+                        <span class="context">Volume : <span class="badge">%.1fL</span></span>
+                        <span class="context">Ebullition : <span class="badge">%.0f min</span></span>
+                    </div>''' % (recipe.style, recipe.efficiency, recipe.volume, recipe.boil)
 
     #Grains
     resultHtml += '<h3>%s</h3>' % QCoreApplication.translate("Export", "Grains et sucres", None, QCoreApplication.UnicodeUTF8)
     resultHtml += '<table class="ingredients">'
     for f in recipe.listeFermentables:
         use = QCoreApplication.translate("Export", "Ajout après ébullition", None, QCoreApplication.UnicodeUTF8) if f.useAfterBoil else ''
-        resultHtml += '<tr><td>%.0f g</td><td>%s</td><td>%s</td></tr>' % (f.amount, f.name, use)
+        resultHtml += '<tr><td><a>%s</a></td><td>%.0f g</td><td>%s</td></tr>' % (f.name, f.amount, use)
     resultHtml += '</table>'
 
     #Houblons
@@ -97,8 +125,9 @@ text-align : center;}
     for h in recipe.listeHops:
         hUI = HopView(h)
         resultHtml += '<tr>'
+        resultHtml += '<td><a>%s</a></td>' % h.name
         resultHtml += '<td>%.0f g</td>' % h.amount
-        resultHtml += '<td>%s (α %.1f %%, %s)</td>' % (h.name, h.alpha, hUI.hopFormDisplay())
+        # resultHtml += '<td>%s (α %.1f %%, %s)</td>' % (h.name, h.alpha, hUI.hopFormDisplay())
         resultHtml += '<td>%.0f min (%s)</td>' % (h.time, hUI.hopUseDisplay())
 
         resultHtml += '</tr>'
@@ -111,8 +140,8 @@ text-align : center;}
         for m in recipe.listeMiscs:
             mUI = MiscView(m)
             resultHtml += '<tr>'
+            resultHtml += '<td><a>%s</a> (%s)</td>' % (m.name, m.type)
             resultHtml += '<td>%.0f g</td>' % m.amount
-            resultHtml += '<td>%s (%s)</td>' % (m.name, m.type)
             resultHtml += '<td>%.0f min (%s)</td>' % (m.time, mUI.miscUseDisplay())
             resultHtml += '</tr>'
         resultHtml += '</table>'
@@ -122,32 +151,34 @@ text-align : center;}
     resultHtml += '<table class="ingredients">'
     for y in recipe.listeYeasts:
         yUI = YeastView(y)
-        resultHtml += '%s<br />' % yUI.yeastDetailDisplay()
+        resultHtml += '<a>%s</a><br />' % yUI.yeastDetailDisplay()
     resultHtml += '</table>'
 
     #Brassage informations
     resultHtml += '<h2>%s</h2>' % QCoreApplication.translate("Export", "Brassage", None, QCoreApplication.UnicodeUTF8)
-    resultHtml += '<p>%s<br />pH : %s</p>' % (recipe.mash.name, recipe.mash.ph)
+    resultHtml += '<p><b>%s</b><br />pH : %s <br/></p>' % (recipe.mash.name, recipe.mash.ph)
 
     #Etapes brassage
-    resultHtml += '<p>'
-    resultHtml += '<b>%s</b><br />' % QCoreApplication.translate("Export", "Étapes : ", None, QCoreApplication.UnicodeUTF8)
     for step in recipe.mash.listeSteps:
         mashStepUI = MashStepView(step)
-        resultHtml += '%s : %s %s %s %s °C %s %s %s<br />' % (step.name,
+        resultHtml += '<p><span class="label label-info">%s</span> : %s %s %s %s °C %s %s %s</p>' % (step.name,
                                                            QCoreApplication.translate("Export", "palier de type", None, QCoreApplication.UnicodeUTF8),
                                                            mashStepUI.mashTypeDisplay(),
                                                            QCoreApplication.translate("Export", "à", None, QCoreApplication.UnicodeUTF8), step.temp,
                                                            QCoreApplication.translate("Export", "pendant", None, QCoreApplication.UnicodeUTF8), step.time,
                                                            QCoreApplication.translate("Export", "minutes", None, QCoreApplication.UnicodeUTF8))
-    resultHtml += '</p>'
+    
 
     #Rincage
-    resultHtml += '<p><b>%s</b>%s °C</p>' % (QCoreApplication.translate("Export", "Rinçage : ", None, QCoreApplication.UnicodeUTF8), recipe.mash.spargeTemp)
+    resultHtml += '<p><span class="label label-inverse">%s</span> : %s °C</p>' % (QCoreApplication.translate("Export", "Rinçage", None, QCoreApplication.UnicodeUTF8), recipe.mash.spargeTemp)
 
     #Notes
     if recipe.recipeNotes is not None:
         resultHtml += '<h2>%s</h2><pre>%s</pre>' % (QCoreApplication.translate("Export", "Notes", None, QCoreApplication.UnicodeUTF8), recipe.recipeNotes)
+
+    
+    #Fin div container
+    resultHtml += ''' </div>'''
 
     resultHtml += '</body></html>'
 
