ce091d5e9523c289d2bac25b15153727d82e63fb
314r <contact.314r@gmail.com>
2014-09-02 12:25:41 +0200

    tri des ingrédients

--- joliebulle/static/controllers/recipes-lib/recipes-lib.js
+++ joliebulle/static/controllers/recipes-lib/recipes-lib.js
@@ -18,15 +18,14 @@ recipesApp.controller('RecipeslibCtrl', ['$scope','$http', '$filter', function (
 
     $scope.openRecipeClicked = function (recipe) {
     	main.viewRecipeLib(recipe.path);
-    	
     };
-    $scope.recipeSelected = function (recipe) {
 
+    $scope.recipeSelected = function (recipe) {
         $scope.active = true;
         $scope.currentRecipe = recipe;
+        $scope.sortRecipe();
         $scope.activeClass = $scope.currentRecipe.path;
         main.viewRecipeLib(recipe.path);
-        
     };
 
     $scope.sortByName = function () {
@@ -35,7 +34,7 @@ recipesApp.controller('RecipeslibCtrl', ['$scope','$http', '$filter', function (
             .sortBy(function(o){return o.name.toLowerCase();})
             .value();
         return $scope.recipes;
-    }
+    };
 
     $scope.sortByBrewer = function () {
         $scope.recipes = _.chain($scope.recipes)
@@ -43,7 +42,15 @@ recipesApp.controller('RecipeslibCtrl', ['$scope','$http', '$filter', function (
             .sortBy(function(o){return o.brewer.toLowerCase();})
             .value();
         return $scope.recipes;
+    };
+
+    $scope.sortRecipe = function () {
+        $scope.currentRecipe.fermentables = _.sortBy($scope.currentRecipe.fermentables, function (o) {return parseInt(o.amount)}).reverse();
+        $scope.currentRecipe.hops = _.sortBy($scope.currentRecipe.hops, function (o) {return parseInt(o.time)}).reverse();
+        $scope.currentRecipe.miscs = _.sortBy($scope.currentRecipe.miscs, function (o) {return parseInt(o.amount)}).reverse();
+        return $scope.currentRecipe;
     }
+
 
 
 
