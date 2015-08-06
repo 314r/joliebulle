397dd45b49d03778f03d6dcb74444e45a2ab859b
314r <contact.314r@gmail.com>
2015-08-06 11:37:42 +0200

    nettoyage

--- joliebulle/static/controllers/recipes-lib/recipes-lib.js
+++ joliebulle/static/controllers/recipes-lib/recipes-lib.js
@@ -16,90 +16,6 @@ recipesApp.controller('RecipeslibCtrl', ['$scope', '$http', '$filter', function 
         $scope.ingredients = ingredients;
         $scope.mashProfiles = profiles.mashes;
     };
-    
-    // $scope.locale_fr = {
-    //     "Grain" : "Grain",
-    //     "Extract" : "Extrait",
-    //     "Dry Extract" : "Extrait Sec",
-    //     "Sugar" : "Sucre",
-    //     "TRUE" : "Après Ebullition",
-    //     "FALSE" : "Brassage",
-    //     "Plug" : "Cône",
-    //     "Leaf" : "Feuille",
-    //     "Pellet" : "Pellet",
-    //     "Boil" : "Ebullition",
-    //     "Dry Hop" : "Dry Hop",
-    //     "First Wort" : "Premier Moût",
-    //     "Mash" : "Empâtage",
-    //     "Aroma" : "Aromatique",
-    //     "Spice" : "Epice",
-    //     "Flavor" : "Arôme",
-    //     "Water Agent" : "Traitement Eau",
-    //     "Herb" : "Herbe",
-    //     "Fining" : "Clarifiant",
-    //     "Other" : "Autre",
-    //     "Primary" : "Primaire",
-    //     "Secondary" : "Secondaire",
-    //     "Bottling" : "Embouteillage",
-    //     "Liquid" : "Liquide",
-    //     "Dry" : "Poudre",
-    //     "Slant" : "Gélose",
-    //     "Culture" : "Culture"
-    // };
-
-    // $scope.translate_fr = function (recipe) {
-    //     recipe.hops.forEach(function (hop) {
-    //         // If formView is in french (not undefined), we translate hop.form to english/beerxml. 
-    //         // If formView is undefined, we translate hop.form to french.
-    //         if (typeof hop.formView !== 'undefined') {
-    //             hop.form = (_.invert($scope.locale_fr))[hop.formView];
-    //         } else {
-    //             hop.formView = $scope.locale_fr[hop.form];
-    //         }
-    //         if (typeof hop.useView !== 'undefined') {
-    //             hop.use = (_.invert($scope.locale_fr))[hop.useView];
-    //         } else {
-    //             hop.useView = $scope.locale_fr[hop.use];
-    //         }
-    //     });
-    //     recipe.fermentables.forEach(function (fermentable) {
-    //         if (typeof fermentable.typeView !== 'undefined') {
-    //             fermentable.type = (_.invert($scope.locale_fr))[fermentable.typeView];
-    //         } else {
-    //             fermentable.typeView = $scope.locale_fr[fermentable.type];
-    //         }
-    //         if (typeof fermentable.afterBoilView !== 'undefined') {
-    //             fermentable.afterBoil = (_.invert($scope.locale_fr))[fermentable.afterBoilView];
-    //         } else {
-    //             fermentable.afterBoilView = $scope.locale_fr[fermentable.afterBoil];
-    //         }
-    //     });
-
-    //     recipe.miscs.forEach(function (misc) {
-    //         if (typeof misc.useView !== 'undefined') {
-    //             misc.use = (_.invert($scope.locale_fr))[misc.useView];
-    //         } else {
-    //             misc.useView = $scope.locale_fr[misc.use];
-    //         }
-    //         if (typeof misc.typeView !== 'undefined') {
-    //             misc.type = (_.invert($scope.locale_fr))[misc.ftypeView];
-    //         } else {
-    //             misc.typeView = $scope.locale_fr[misc.type];
-    //         }
-
-    //     });
-
-    //     recipe.yeasts.forEach(function (yeast) {
-    //         if (typeof yeast.formView !== 'undefined') {
-    //             yeast.form = (_.invert($scope.locale_fr))[yeast.formView];
-    //         } else {
-    //             yeast.formView = $scope.locale_fr[yeast.form];
-    //         }
-    //     });
-
-
-    //     return recipe;
-    // };
 
     $scope.deleteLib = function (recipe) {
 		$scope.recipes.splice($scope.recipes.indexOf(recipe), 1);
@@ -184,9 +100,7 @@ recipesApp.controller('RecipeslibCtrl', ['$scope', '$http', '$filter', function 
     };
 
     $scope.calcProfile = function (recipe) {
-        console.log(recipe);
         recipe = translate.translate_fr(recipe);
-        
         recipe.ebc = Math.round(beerCalc.ebc(recipe.fermentables, recipe.volume));
         recipe.og = (Math.round(beerCalc.originalGravity(recipe) * 1000) / 1000).toFixed(3);
         recipe.fg = (Math.round(beerCalc.finalGravity(recipe) * 1000) / 1000).toFixed(3);
