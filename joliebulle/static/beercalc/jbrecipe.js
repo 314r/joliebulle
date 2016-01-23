bb93eeced69c424056f8c3828c1e8a3793eaf162
314r <contact.314r@gmail.com>
2016-01-23 15:34:04 +0100

    base nouvelle recette

---
+++ joliebulle/static/beercalc/jbrecipe.js
@@ -0,0 +1,22 @@
+/*jslint nomen: true */
+var jbrecipe = (function () {
+    "use strict";
+    var recipe;
+    
+    return {
+        newRecipe : function () {
+            recipe = {};
+            recipe.name = "Nouvelle Recette";
+            recipe.volume = "0";
+            recipe.style = "generic";
+
+            
+            
+        
+        }
+    
+    
+    
+    };
+    
+}());
